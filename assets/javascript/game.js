ecbe5205bf02e26b0540b1b9fca414043605e78c
Sam <samlconsidine@gmail.com>
2020-01-11 22:27:14 -0500

    Sending JS to display spaces of the guess word to the DOM.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,26 +12,31 @@ for (var i = 0; i < word.length; i++) {
 console.log (word.length);
 console.log (answerArray);
 
-// Game loop
-while (remainingLetters > 0) {
-    // shows progress
-    alert(answerArray.join(" "));
+var div = document.getElementById("guessWord");
+div.innerHTML = answerArray.join(" ");
 
-    // get player's guess
-    var guess = prompt("Guess a letter");
-    if (guess === null) {
-        break;
-    } else {
-        for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-            }
-        }
-    }
-}
+// document.getElementById('guessWord').textContent = answerArray;
 
-alert(answerArray.join(" "));
+// // Game loop
+// while (remainingLetters > 0) {
+//     // shows progress
+//     alert(answerArray.join(" "));
+
+//     // get player's guess
+//     var guess = prompt("Guess a letter");
+//     if (guess === null) {
+//         break;
+//     } else {
+//         for (var j = 0; j < word.length; j++) {
+//             if (word[j] === guess) {
+//                 answerArray[j] = guess;
+//             }
+//         }
+//     }
+// }
+
+// alert(answerArray.join(" "));
 
 
-var winCounter = 0;
-document.getElementById("winTally").innerHTML = wins;
+// var winCounter = 0;
+// document.getElementById("winTally").innerHTML = wins;
