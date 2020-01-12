0ef070aaa3e5c45602199e41220cfd936fed9dfc
Sam <samlconsidine@gmail.com>
2020-01-11 22:05:13 -0500

    Added random word generator for game as well as for loop to create blank spaces for each letter in word.

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var words = ["Harry", "Hermione", "Ron", "Hogwarts", "Voldemort", "Dumbledore", "Hagrid", "Quidditch"]; // possible word options
+
+// Pick a random word
+var word = words[Math.floor(Math.random() * words.length)];
+console.log(word);
+
+// Set up answer array 
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
+};
+console.log (word.length);
+console.log (answerArray);
+
+// Game loop
+while (remainingLetters > 0) {
+    // shows progress
+    alert(answerArray.join(" "));
+
+    // get player's guess
+    var guess = prompt("Guess a letter");
+    if (guess === null) {
+        break;
+    } else {
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === guess) {
+                answerArray[j] = guess;
+            }
+        }
+    }
+}
+
+alert(answerArray.join(" "));
+
+
+var winCounter = 0;
+document.getElementById("winTally").innerHTML = wins;
